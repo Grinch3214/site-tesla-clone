e2b29beadd194814246fa0da0a0bd08d3447279b
Grinch3214 <scorpiomax90@gmail.com>
2022-09-27 23:46:38 +0300

    addind sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,20 @@ module.exports = {
 		},
     extend: {
 			backgroundImage: theme => ({
-				'model-3': 'url("/images/m3-desktop.jpg")',
-				'mobile-model-3': 'url("/images/m3-mobile.jpg")'
+				'model-3': 'url("../images/m3-desktop.jpg")',
+				'mobile-model-3': 'url("../images/m3-mobile.jpg")',
+				'model-s': 'url("../images/model-s.jpeg")',
+				'mobile-model-s': 'url("../images/mobile-model-s.jpeg")',
+				'model-y': 'url("../images/model-y.jpeg")',
+				'mobile-model-y': 'url("../images/mobile-model-y.jpeg")',
+				'model-x': 'url("../images/model-x.jpeg")',
+				'mobile-model-x': 'url("../images/mobile-model-x.jpeg")',
+				'solar-panels': 'url("../images/solar-panels.jpeg")',
+				'mobile-solar-panels': 'url("../images/mobile-solar-panels.jpeg")',
+				'solar-roof': 'url("../images/solar-roof.jpeg")',
+				'mobile-solar-roof': 'url("../images/mobile-solar-roof.jpeg")',
+				'accessories': 'url("../images/accessories.jpeg")',
+				'mobile-accessories': 'url("../images/mobile-accessories.jpeg")',
 			}),
 		},
   },
